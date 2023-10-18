b7b8d18c19c425bc0a6fdcd9859475c4c51809ff
Kevin Lim <kevin.lim6459@gmail.com>
2023-10-17 20:54:55 -0500

    Saving current progress

--- Assets/Scripts/SendEmoticon.cs
+++ Assets/Scripts/SendEmoticon.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SendEmoticon : MonoBehaviour
 {
+    private Touch currentTouch;
+    private float startTime;
+    private Vector2 startPosition;
+
     //See Touch class in unity documentation
     //https://docs.unity3d.com/ScriptReference/Touch.html
     public struct GestureData
@@ -24,9 +28,55 @@ public class SendEmoticon : MonoBehaviour
     //any gesture that occurs, add to the gestureQueue. Thats all that has to happen in here.
     //
     //Look at reference for touch I added above.
+
+    public struct TapData
+    {
+        float startTime;
+        float endTime;
+    }
+
+    private Queue<TapData> tapQueue = new Queue<TapData>();
+
     void Update()
     {
+        if (Input.touchCount > 0) {
+            currentTouch = Input.GetTouch(0);
+            
+            if (currentTouch.position.Y < buttonY) {return;}
 
+            if (currentTouch.phase == TouchPhase.Began) {
+                startTime = Time.time;
+            } else if (currentTouch.phase == TouchPhase.Ended) {
+                TapData currentTap = new TapData();
+                currentTap.startTime = startTime;
+                currentTap.endTime = Time.time;
+
+                tapQueue.Enqueue(currentTap);
+            }
+            """
+            // Check to make sure touch position is valid
+            if (currentTouch.position.Y < buttonY) {return;}
+
+            // Initialize variables in 'Began' phase
+            if (currentTouch.phase == TouchPhase.Began) {
+                startTime = Time.time;
+                startPosition.X = currentTouch.position.X;
+                startPosition.Y = currentTouch.position.Y;
+            // Create new GestureData struct with appropriate data in 'Ended' phase, and Enqueue
+            } else if (currentTouch.phase == TouchPhase.Ended) {
+                GestureData currentGesture = new GestureData();
+
+                currentGesture.deltaPosition.X = currentTouch.position.X - startPosition.X;
+                currentGesture.deltaPosition.Y = currentTouch.position.Y - startPosition.Y;
+                currentGesture.position.X = startPosition.X;
+                currentGesture.position.Y = startPosition.Y;
+                currentGesture.deltaTime = Time.time - startTime;
+                
+                gestureQueue.Enqueue(currentGesture);
+                
+            }
+            """
+        }
     }
 
     public GestureData[] GetGestures()
